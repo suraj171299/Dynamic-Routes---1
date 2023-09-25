b8c3dabc9b3a0ccb5e69b55ac2d34854f1710096
suraj171299 <surajpadalkar1999@gmail.com>
2023-09-25 11:36:00 +0530

    Final Project express js

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const Sequelize =require('sequelize');
+const sequelize= new Sequelize('node-project','root','8879',{
+    dialect:'mysql',
+    host:'localhost'
+});
+
+
+
+
+module.exports = sequelize;
